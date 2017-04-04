96f036f36cf3288b67b4cceccad9c940a8202aed
DragosB <dragosbad2009@gmail.com>
2017-04-04 20:44:23 +0300

    Update

---
+++ src/main/controller/LoginControl.java
@@ -0,0 +1,116 @@
+package main.controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import main.main.Main;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Dragos on 4/4/2017.
+ */
+public class LoginControl implements Initializable {
+
+    @FXML
+    private TextField userField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button loginButton;
+    @FXML private RadioButton cmRadio;
+    @FXML private RadioButton attendantRadio;
+    @FXML private RadioButton authorRadio;
+    private ToggleGroup group;
+
+    public LoginControl()
+    {
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        passwordField.setPromptText("Your password");
+        userField.setPromptText("Your username");
+
+        this.group = new ToggleGroup();
+
+        cmRadio.setToggleGroup(group);
+        cmRadio.setSelected(true);
+
+        attendantRadio.setToggleGroup(group);
+
+        authorRadio.setToggleGroup(group);
+    }
+
+    public void initManager(final Main loginManager)
+    {
+        loginButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                    int response = 0;
+                    String userName = userField.getText();
+                    String password = passwordField.getText();
+                    String selected = group.getSelectedToggle().getUserData().toString();
+                    if (userName == null) {
+                        showErrorMessage("Dati un username");
+                    } else if (password == null) {
+                        showErrorMessage("Dati o parola");
+                    } else
+                        {
+                            if (selected.compareTo("CM") == 0)
+                            {
+                                //aici va fi legagura cu repository-ul CM
+                                // (ar fi o chestie sa returneze in respone valid,
+                                // daca e corecta parola, sau invalid in caz contrar
+                                // (e o verificare pt response mai jos)
+                            }
+                            else if (selected.compareTo("Author") == 0)
+                            {
+                                //aici va fi legagura cu repository-ul author
+                                // (ar fi o chestie sa returneze in respone valid,
+                                // daca e corecta parola, sau invalid in caz contrar
+                                // (e o verificare pt response mai jos)
+                            }
+                            else if (selected.compareTo("Attendant") == 0)
+                            {
+                                //aici va fi legagura cu repository-ul attendant
+                                // (ar fi o chestie sa returneze in respone valid,
+                                // daca e corecta parola, sau invalid in caz contrar
+                                // (e o verificare pt response mai jos)
+                            }
+                            if (response == 0) {
+                                showErrorMessage("Username sau parola invalida");
+                                initManager(loginManager);
+                            }
+                            else
+                            {
+                                loginManager.authenticated(response);
+                            }
+                    }
+            }
+        });
+    }
+
+    private static void showMessage(Alert.AlertType type)
+    {
+        Alert message=new Alert(type);
+        message.setHeaderText("Succes");
+        message.setContentText("Succes");
+        message.showAndWait();
+    }
+
+    private static void showErrorMessage(String text)
+    {
+        Alert message=new Alert(Alert.AlertType.ERROR);
+        message.setTitle("Mesaj eroare");
+        message.setContentText(text);
+        message.showAndWait();
+    }
+}
---
+++ src/main/main/Main.java
@@ -0,0 +1,90 @@
+package main.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import main.controller.LoginControl;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+
+/**
+ * Created by Dragos on 4/4/2017.
+ */
+public class Main extends Application {
+    private FXMLLoader loader;
+    private FXMLLoader loader2;
+    private Stage primaryStage;
+    private AnchorPane rootLayout1;
+    private TabPane rootLayout2;
+
+    private static void execute(String sql) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws ClassNotFoundException, SQLException {
+        this.primaryStage = primaryStage;
+        loader = new FXMLLoader();
+        loader2 = new FXMLLoader();
+        LoginView();
+    }
+
+    public void authenticated(int response)
+    {
+        if (response == 1)
+        {
+            MainViewCM();
+        }
+        else if (response == 2)
+        {
+            MainViewAuthor();
+        }
+        else if (response ==3)
+        {
+            MainViewAttendant();
+        }
+    }
+
+    private void LoginView() throws ClassNotFoundException, SQLException {
+        try {
+            String pathToFxml = "src/main/resources/LoginWindow.fxml";
+            URL fxmlUrl = new File(pathToFxml).toURI().toURL();
+            loader.setLocation(fxmlUrl);
+            LoginControl controlLogin = new LoginControl();
+            loader.setController(controlLogin);
+            rootLayout1 = loader.load();
+            Scene scene = new Scene(rootLayout1);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            controlLogin.initManager(this);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void MainViewAuthor()
+    {
+
+    }
+
+    private void MainViewAttendant()
+    {
+
+    }
+
+    private void MainViewCM()
+    {
+
+    }
+}
