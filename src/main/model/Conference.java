c34a1e7a9df8fbb4e738012aa6c1df21042d86c4
stefanvacareanu <vacareanu_stefan@yahoo.com>
2017-05-10 22:14:05 +0300

    Comittee GUI design + model for conference and section

--- src/main/model/Conference.java
+++ src/main/model/Conference.java
@@ -5,4 +5,47 @@ package main.model;
  */
 public class Conference
 {
+    int idConference;
+    String name;
+    int noParticipants;
+
+
+    public Conference(int idConference,String name, int noParticipants) {
+        this.idConference = idConference;
+        this.name = name;
+        this.noParticipants = noParticipants;
+    }
+
+    @Override
+    public String toString() {
+        return "Conference{" +
+                "idConference=" + idConference +
+                ", name='" + name + '\'' +
+                ", noParticipants=" + noParticipants +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdConference() {
+        return idConference;
+    }
+
+    public void setIdConference(int idConference) {
+        this.idConference = idConference;
+    }
+
+    public int getNoParticipants() {
+        return noParticipants;
+    }
+
+    public void setNoParticipants(int noParticipants) {
+        this.noParticipants = noParticipants;
+    }
 }
